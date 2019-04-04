1fe9fa4b3fc2757863a2f328e8bb12de00b54ab3
Weverton <wevertonwdm@hotmail.com>
2019-04-04 17:18:37 -0300

    arrumando erro
    atividade 5 02

--- Pratica5/pratica05exercicio02.js
+++ Pratica5/pratica05exercicio02.js
@@ -1,10 +1,6 @@
-function ImpaPar() {
-    var from=document.getElementById("primeiroNumero").value;
-    var n1 = from % 2;
-    if (n1 == 0){
-        document.getElementById("resposta").innerHTML= "PAR";
-    } else {
-        document.getElementById("resposta").innerHTML= "IMPAR";
-    }
-    document.getElementById("resposta").innerHTML += "---- " + from;
+function somar (){
+    var n1=document.getElementById("primeiroNumero").value;
+    var n2=document.getElementById("segundoNumero").value;
+    var soma= parseInt (n1)+ parseInt (n2);
+    document.getElementById ("soma").innerHTML=soma;
 }
